d208bacefe6453191ff8f5e154f491edf689d53b
tylim <limteikyee@gmail.com>
2022-04-03 06:02:46 +0800

    1st commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	arrowParens: 'avoid',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'es5',
+	semi: false,
+	bracketSameLine: false,
+	useTabs: true,
+	endOfLine: 'auto',
+}
